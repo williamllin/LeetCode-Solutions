a8eb8cc38721edc25bc4e0c2f3b9ca497a36941d
William Lin <willyrblin@gmail.com>
2026-05-12 13:26:30 +0800

    Time: 1098 ms (24.16%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1415-students-and-examinations/students-and-examinations.sql
@@ -0,0 +1,23 @@
+# Write your MySQL query statement below
+select st.student_id, st.student_name, su.subject_name, count(e.subject_name) as attended_exams
+from students st
+    cross join subjects su
+    left join examinations e
+        on st.student_id = e.student_id
+        and su.subject_name = e.subject_name
+    
+group by  st.student_id, st.student_name, su.subject_name
+order by st.student_id, su.subject_name
+
+#count(e.subject_name) so that there's 'NULL' to be displayed as '0'
+#cross join makes every students match each subject, doesn't matter if they took it or not
+
+
+
+
+
+
+
+
+
+
