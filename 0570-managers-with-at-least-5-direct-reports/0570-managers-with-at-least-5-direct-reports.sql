7c78fe340d944528ad3cdd7c1cb406bb2ff59d7f
William Lin <willyrblin@gmail.com>
2026-05-13 17:24:06 +0800

    Time: 367 ms (31.66%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -5,14 +5,7 @@ from employee e
     join (select id, name #inner join to void manager id that doesn't exist in this company(ex:100)
         from employee e) ma
             on e.managerId = ma.id
-group by ma.id, ma.name #group by name since name is the main, group by id since people might have same name
+group by ma.name, ma.id #group by name since name is the main, group by id since people might have same name
 having count(e.id) >=5 
 #use employee id to count instead manager id, avoid null employee id(some managers might not have employees to match)
-
-
-
-
-
-
-
 
