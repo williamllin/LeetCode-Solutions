b0f8b22eb3898cabaf2fedd1ab0b3617d1000578
William Lin <willyrblin@gmail.com>
2026-05-13 17:16:02 +0800

    Time: 390 ms (17.69%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,14 +1,13 @@
 # Write your MySQL query statement below
 
-
-
 select ma.name
 from employee e
-    join (select id, name
-          from employee) ma 
-            on ma.id = e.managerId
-group by ma.id, ma.name
-having count(e.id) >=5
+    join (select id, name #inner join to void manager id that doesn't exist in this company(ex:100)
+        from employee e) ma
+            on e.managerId = ma.id
+group by ma.id, ma.name #group by name since name is the main, group by id since people might have same name
+having count(e.id) >=5 
+#use employee id to count instead manager id, avoid null employee id(some managers might not have employees to match)
 
 
 
