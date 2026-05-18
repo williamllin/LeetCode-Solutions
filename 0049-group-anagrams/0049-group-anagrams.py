9c9dad3c88917f6006f91e324149be8b036d5506
William Lin <willyrblin@gmail.com>
2026-05-18 17:16:10 +0800

    Time: 15 ms (45.03%), Space: 22 MB (67.58%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -2,11 +2,12 @@
 #from collections import defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        anagram_map = defaultdict(list) #use this instead of {} so that when new key(aet) added will add new [] automatically
+        anagram_map = defaultdict(list) 
+        #use this instead of {} so that when new key(aet) added will add new [] automatically
         
+
         for s in strs:
-            #Sort the characters of the string and join them back into a string
-            sorted_s = "".join(sorted(s)) 
+            sorted_s = "".join(sorted(s)) # "".join(['a','e','t']) -> aet
             anagram_map[sorted_s].append(s)
         return list(anagram_map.values())
 
