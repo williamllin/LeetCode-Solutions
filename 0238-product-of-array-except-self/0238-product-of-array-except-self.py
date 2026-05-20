dd93ef3a80d53c84d3c534dcaa1ca1ed8ea8d542
William Lin <willyrblin@gmail.com>
2026-05-20 17:02:56 +0800

    Time: 19 ms (77.18%), Space: 25.2 MB (89.64%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,20 +1,26 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        #初始化輸出的答案陣列，全部填 1
+        #初始化輸出的答案陣列 全部填 1
         res = [1] * n
         
-        #步驟 1：計算前綴乘積（左邊所有數字的乘積）
-        #prefix_product 用來記錄目前位置左邊所有人的累積乘積
+        #計算前綴乘積（左邊數字乘積）
+        #prefix_product用來記錄目前位置左邊所有的累積乘積
         prefix_product = 1
         for i in range(n):
             res[i] = prefix_product
-            prefix_product *= nums[i]#更新前綴乘積，留給下一個數字用
+            prefix_product *= nums[i]#更新前綴乘積 留給下一個數字用
             
-        # 步驟 2：從右往左一邊計算後綴乘積（右邊），一邊直接乘進 res 陣列裡
+        #從右往左一邊計算後綴乘積（右邊） 一邊直接乘進res陣列裡
         suffix_product = 1
         for i in range(n - 1, -1, -1):
             res[i] *= suffix_product #將目前的左積直接乘以剛剛累積的右積
-            suffix_product *= nums[i] #更新後綴乘積，留給左邊的下一個數字用
+            suffix_product *= nums[i] #更新後綴乘積 留給左邊的下一個數字用
             
         return res
+
+
+        #for迴圈邏輯: 先把目前左邊累積的成果（prefix_product）送給 res[i]，送完之後，再把現在的 nums[i] 乘進去，留給下一個人用。
+        
+        #當 i = 0 (數字是 2)：res[0] = prefix_product $\rightarrow$ 此時 prefix_product 是 1（因為 2 的左邊沒有任何人，所以是 1）。此時 res = [1, 1, 1, 1]。更新留給下一個人的：prefix_product = 1 * 2 = 2。
+        #當 i = 1 (數字是 3)：res[1] = prefix_product $\rightarrow$ 把剛才算好左邊的 2 送給它。此時 res = [1, 2, 1, 1]。更新留給下一個人的：prefix_product = 2 * 3 = 6。
