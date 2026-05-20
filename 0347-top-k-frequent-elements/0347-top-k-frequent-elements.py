79faad8283c27e400c19c8205ee3c827785051ed
William Lin <willyrblin@gmail.com>
2026-05-20 16:40:49 +0800

    Time: 7 ms (51.49%), Space: 22.7 MB (95.02%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,6 +1,9 @@
 #import heapq
 #from collections import Counter
 #from typing import List
+
+
+#當我們把元素丟進 Heap 時，如果 Heap 的大小超過了 $K$，就把此時「頻率最低」的頂端元素踢出去（heappop）。這樣一路走到最後，留在 Heap 裡面的，絕對就是全場最高頻的前 $K$ 個元素。
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         #Count the frequency of each number
@@ -8,7 +11,7 @@ class Solution:
         
         #Maintain a Min-Heap of size K
         heap = []
-        for num, freq in count.items():
+        for num, freq in count.items():#items() so that get key and value
             #Python's heapq sorts by the first element of the tuple (freq)
             heapq.heappush(heap, (freq, num))
 
