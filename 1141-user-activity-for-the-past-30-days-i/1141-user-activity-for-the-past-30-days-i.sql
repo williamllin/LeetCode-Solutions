ec36d837e927312194b22b870f7af1e099c3ab3c
William Lin <willyrblin@gmail.com>
2026-05-21 12:17:55 +0800

    Time: 502 ms (46.16%), Space: 0B (100%) - LeetHub

--- 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
+++ 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
@@ -2,4 +2,6 @@
 select activity_date as day, count(distinct user_id) as active_users
 from activity
 where activity_date BETWEEN '2019-06-28' AND '2019-07-27'
+#WHERE DATEDIFF('2019-07-27', activity_date) >= 0  to prevent ex:2019/8/1 -> -5<=30 
+  #AND DATEDIFF('2019-07-27', activity_date) < 30
 group by day
