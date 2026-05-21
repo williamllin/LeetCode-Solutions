0333a271dd3ec4fe3fc36996679e8fef94d39354
William Lin <willyrblin@gmail.com>
2026-05-21 15:51:40 +0800

    Time: 253 ms (91.85%), Space: 67 MB (99.22%) - LeetHub

--- 0196-delete-duplicate-emails/0196-delete-duplicate-emails.py
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
 def delete_duplicate_emails(person: pd.DataFrame) -> None:
+    #inplace because question says modify in place
+    #sort first, and keep the first id with same email(ex:id 1 and 3 is same guy)
     person.sort_values(by='id',ascending=True,inplace=True)
     person.drop_duplicates(subset = ['email'], keep='first', inplace=True)
