2d888f872769e09d0e0e60703ac3cc49d0730487
William Lin <willyrblin@gmail.com>
2026-05-26 16:00:32 +0800

    Time: 341 ms (61.58%), Space: 89.9 MB (80.24%) - LeetHub

--- 1907-count-salary-categories/1907-count-salary-categories.py
+++ 1907-count-salary-categories/1907-count-salary-categories.py
@@ -1,15 +1,12 @@
 import pandas as pd
 
 def count_salary_categories(accounts: pd.DataFrame) -> pd.DataFrame:
+    low_sal = (accounts['income']<20000).sum()#sum() will sum up all True(1) in condition
+    avg_sal = ((accounts['income']>=20000) & (accounts['income']<=50000)).sum()
+    hig_sal = (accounts['income']>50000).sum()
 
-    low_count = (accounts['income'] < 20000).sum() #sum() will sum up number of True (1)
-    average_count = ((accounts['income'] >= 20000) & (accounts['income'] <= 50000)).sum()
-    high_count = (accounts['income'] > 50000).sum()
-    
-
-    res = pd.DataFrame({
+    result = pd.DataFrame({
         'category': ['Low Salary', 'Average Salary', 'High Salary'],
-        'accounts_count': [low_count, average_count, high_count]
+        'accounts_count': [low_sal, avg_sal, hig_sal]
     })
-    
-    return res
+    return result
