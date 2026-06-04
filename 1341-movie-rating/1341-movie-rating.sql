6ec9aa5e7365be18de64795093ed525392c874f0
William Lin <willyrblin@gmail.com>
2026-06-04 16:09:31 +0800

    Time: 1467 ms (24.15%), Space: 0B (100%) - LeetHub

--- 1341-movie-rating/1341-movie-rating.sql
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -1,17 +1,17 @@
-# Write your MySQL query statement below
+
 (
 select u.name as results
 from movierating mr
 inner join users u
     on mr.user_id = u.user_id
 group by mr.user_id, u.name
-order by count(mr.movie_id) desc, u.name asc
+order by count(movie_id) desc, u.name asc
 limit 1
 )
 
 union all
 
-(
+(   
 select m.title
 from movierating mr
 inner join movies m
@@ -19,5 +19,20 @@ inner join movies m
 where mr.created_at between '2020-02-01' and '2020-02-29'
 group by mr.movie_id, m.title
 order by avg(mr.rating) desc, m.title asc
-limit 1 
+limit 1
 )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
