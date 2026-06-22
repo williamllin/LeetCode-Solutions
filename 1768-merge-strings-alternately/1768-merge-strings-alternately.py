d8d414ee11038edc5c8b6b80f9613f71b31e265d
William Lin <willyrblin@gmail.com>
2026-06-22 13:00:22 +0800

    Time: 49 ms (28.85%), Space: 19.3 MB (17.35%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -4,8 +4,7 @@ class Solution:
         result = []
         i, j = 0, 0 #two pointers, pointing at the start of word1 and word2
 
-        #if both have amount of words, take turns
-        #ex: word1=ab, word2=pqrs
+        #ex: word1="ab", word2="pqrs"
         while i < len(word1) and j < len(word2):
             result.append(word1[i])
             result.append(word2[j])
@@ -13,7 +12,7 @@ class Solution:
             j+=1
 
         #(following the top codes)if one still have words left, stick the rest with no space
-        #ex: word1 is done, word2 still have rs -> append(word2[j:])
+        #ex: word1 is done, word2 still have "rs" -> append(word2[j:])
         if i < len(word1):
             result.append(word1[i:])
         if j < len(word2):
