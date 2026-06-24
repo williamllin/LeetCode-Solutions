1d30f49c6f009bc0304616c320f75dd5855fdeb1
William Lin <willyrblin@gmail.com>
2026-06-24 16:50:24 +0800

    Time: 6 ms (83.31%), Space: 19.4 MB (78.46%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -1,26 +1,21 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
         length = len(flowerbed)
-        
-        for i in range(length):
-            #if already planted, skip it
-            if flowerbed[i] == 1:
+
+        for i in range(length): #if spot planted, move on
+            if flowerbed[i]==1:
                 continue
-                
-            #spot that is empty at left([0] and [i-1] is '0')
-            left_empty = (i == 0) or (flowerbed[i - 1] == 0)
-            
-            #spot that is empty at right([last one] and [i+1] is '0')
-            right_empty = (i == length - 1) or (flowerbed[i + 1] == 0)
-            
-            #if left and right both empty
-            if left_empty and right_empty:
-                flowerbed[i] = 1  #plant one flower 0->1
-                n -= 1            #one less flower on hand
-                
-                #if no flower then done, so no need to scan through the rest spots
-                if n <= 0:
+
+            left_empty = (i==0) or (flowerbed[i-1]==0) #left to the spot empty
+            right_empty = (i == length-1) or (flowerbed[i+1]==0) #right to the spot empty
+
+            if left_empty and right_empty: #both sides empty -> plant a flower and -1 flower in hand
+                flowerbed[i]=1
+                n-=1
+
+                if n<=0: #for performance optimization: if no flower, no need to scan the rest spots
                     return True
-                    
-        #if planted all then True, else  false
-        return n <= 0
+        return n<=0
+
+
+        
