f1b6c4a47accca9c209f50b50fa3f3f44a426e1f
William Lin <willyrblin@gmail.com>
2026-06-24 15:42:37 +0800

    Time: 47 ms (45.6%), Space: 19.4 MB (17.21%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -12,7 +12,7 @@ class Solution:
             j+=1
 
         #(following the top codes)if one still have words left, stick the rest with no space
-        #ex: word1 is done, word2 still have "rs" -> append(word2[j:])
+        #ex: word1 is done, word2 still have "rs" ->append(word2[j:])
         if i < len(word1):
             result.append(word1[i:])
         if j < len(word2):
