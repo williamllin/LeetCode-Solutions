52f281887f024eaa236a3d427ff82dc065444826
William Lin <willyrblin@gmail.com>
2026-06-25 12:45:37 +0800

    Time: 360 ms (54.83%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -20,3 +20,28 @@ group by ma.name, ma.id #group by name since name is the main, group by id since
 having count(e.id) >=5 
 #use employee id to count instead manager id, avoid null employee id(some managers might not have employees to match)
 
+/*
+table after join:
+e.id   e.name   e.dep   e.magId   ma.name   ma.id
+102    dan      a       101       john      101
+103    james    a       101       john      101
+104    amy      a       101       john      101
+105    anne     a       101       john      101
+106    ron      b       101       john      101
+*/
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
