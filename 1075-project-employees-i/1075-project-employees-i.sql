30abf46965da841db81fac79fffec807bea5a902
William Lin <willyrblin@gmail.com>
2026-06-25 16:45:32 +0800

    Time: 495 ms (94.19%), Space: 0B (100%) - LeetHub

--- 1075-project-employees-i/1075-project-employees-i.sql
+++ 1075-project-employees-i/1075-project-employees-i.sql
@@ -3,3 +3,12 @@ from project p
     left join employee e
         on p.employee_id = e.employee_id
 group by p.project_id
+
+/*
+**project id   emp id        emp id        name     **exp.years
+| 1           | 1           | 1           | Khaled | 3                |
+| 1           | 2           | 2           | Ali    | 2                |
+| 1           | 3           | 3           | John   | 1                |
+| 2           | 1           | 1           | Khaled | 3                |
+| 2           | 4           | 4           | Doe    | 2                |
+*/
