ab5b4b6a99a43a5cd5c3071bef1682b2030babc2
William Lin <willyrblin@gmail.com>
2026-06-25 16:14:22 +0800

    Time: 251 ms (97.15%), Space: 0B (100%) - LeetHub

--- 0620-not-boring-movies/0620-not-boring-movies.sql
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -2,5 +2,6 @@
 select id, movie, description, rating
 from cinema
 where id%2 != 0
-    and description not like 'boring' #'is not' -> for NULL, 'not like' -> for varchar
+    and description not like 'boring' #'is not' ->for NULL, 'not like' ->for varchar
 order by rating desc
+
