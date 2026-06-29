62a7b9cf1dded2b39bb25c167ede60b003e5679a
William Lin <willyrblin@gmail.com>
2026-06-29 12:10:08 +0800

    Time: 951 ms (78.58%), Space: 0B (100%) - LeetHub

--- 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
+++ 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
@@ -1,4 +1,4 @@
-select r.contest_id, round(count(r.user_id)/(select count(*) from users)*100 ,2) as percentage
-from register r 
+select r.contest_id, round(count(r.user_id)/(select count(*) from users)*100,2) as percentage
+from register r
 group by r.contest_id
 order by percentage desc, contest_id asc
