bcb917f2e963eb8485347542af2d676616520a3b
William Lin <willyrblin@gmail.com>
2026-06-29 12:50:44 +0800

    Time: 704 ms (29.07%), Space: 0B (100%) - LeetHub

--- 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -1,5 +1,3 @@
-# Write your MySQL query statement below
-
 select 
     round(avg(case when order_date = customer_pref_delivery_date then 1 else 0 end) * 100, 2) as immediate_percentage 
 #this still counts the non-first date, so need subQ to select each customer first date
@@ -10,11 +8,3 @@ where (customer_id, order_date) in (
     from delivery
     group by customer_id
 )
-
-
-
-
-
-
-
-
