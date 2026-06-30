29a2f29bda5fa27b3a62a471cf8144b9b35f2a7b
William Lin <willyrblin@gmail.com>
2026-06-30 11:26:22 +0800

    Time: 593 ms (45.45%), Space: 0B (100%) - LeetHub

--- 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -1,11 +1,9 @@
-# Write your MySQL query statement below
-
-select round(count(a2.player_id)/count(a1.player_id) ,2) as fraction
+select round(count(a2.player_id)/count(a1.player_id),2) as fraction
 from (
     select player_id, min(event_date) as first_login
-    from activity 
+    from activity
     group by player_id
-) a1 left join activity a2 
+) a1 left join activity a2
         on a1.player_id = a2.player_id
-        and datediff(a2.event_date, a1.first_login) =1
+        and datediff(a2.event_date, a1.first_login) = 1
 
