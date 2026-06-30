8c57013093073a830355ca3c4b27a61f089f7c05
William Lin <willyrblin@gmail.com>
2026-06-30 16:16:36 +0800

    Time: 628 ms (83.22%), Space: 0B (100%) - LeetHub

--- 1731-the-number-of-employees-which-report-to-each-employee/1731-the-number-of-employees-which-report-to-each-employee.sql
+++ 1731-the-number-of-employees-which-report-to-each-employee/1731-the-number-of-employees-which-report-to-each-employee.sql
@@ -6,3 +6,11 @@ from employees e1
 group by e1.employee_id, e1.name
 order by e1.employee_id
 
+/*
+e1.emp_id   e1.name   e1.age | e2.emp_id  e2.name   e2.reports_to  e2.age
+9           hercy     43       6          alice     9              41
+9           hercy     43       4          bob       9              36
+6           alice     41       6          alice (this row won't exist)
+4           bob       36       4          bob (this row won't exist)     
+2           winston   37       2          winston (this row won't exist)
+*/
