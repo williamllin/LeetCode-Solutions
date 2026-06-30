ebb5834d5efe6e1f3ea67c6c9b824818f651857f
William Lin <willyrblin@gmail.com>
2026-06-30 11:53:27 +0800

    Time: 546 ms (84.31%), Space: 0B (100%) - LeetHub

--- 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -1,4 +1,3 @@
-/*
 select round(count(a2.player_id)/count(a1.player_id),2) as fraction
 from (
     select player_id, min(event_date) as first_login
@@ -9,24 +8,4 @@ from (
         and datediff(a2.event_date, a1.first_login) = 1
 #分母:所有人的第一次登入(a1)
 #分子:跟第一次登入差一天的才算(a2)
-*/
-
-select round(count(a1.player_id)/count(a2.player_id),2) as fraction
-from activity a1
-    right join (
-        select player_id, min(event_date) as first_login
-        from activity
-        group by player_id 
-    ) a2 on a1.player_id = a2.player_id
-            and datediff(a1.event_date, a2.first_login) = 1
-
-
-
-
-
-
-
-
-
-
 
