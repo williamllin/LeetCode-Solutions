04c33e7d40e468fcf4dccdbe8090f2eab0898816
William Lin <willyrblin@gmail.com>
2026-06-30 11:11:52 +0800

    Time: 627 ms (75.5%), Space: 0B (100%) - LeetHub

--- 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -1,3 +1,4 @@
+/*
 select 
     round(avg(case when order_date = customer_pref_delivery_date then 1 else 0 end) * 100, 2) as immediate_percentage 
 #this still counts the non-first date, so need subQ to select each customer first date
@@ -8,4 +9,24 @@ where (customer_id, order_date) in (
     from delivery
     group by customer_id
 )
+*/
+
+select round(avg(case when order_date = customer_pref_delivery_date then 1 else 0 end)*100,2)
+    as immediate_percentage
+from delivery
+where (customer_id, order_date) in (
+    select customer_id, min(order_date)
+    from delivery
+    group by customer_id
+)
+
+
+
+
+
+
+
+
+
+
 
