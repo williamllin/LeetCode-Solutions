bbcf3f1a3517001a9d6aeaf301a61459839f2cee
William Lin <willyrblin@gmail.com>
2026-07-02 16:06:21 +0800

    Time: 1599 ms (14.19%), Space: 0B (100%) - LeetHub

--- 1341-movie-rating/1341-movie-rating.sql
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -1,37 +1,23 @@
-
 (
 select u.name as results
-from movierating mr
-inner join users u
-    on mr.user_id = u.user_id
-group by mr.user_id, u.name
-order by count(movie_id) desc, u.name asc
+from users u
+inner join movierating mo
+    on u.user_id = mo.user_id
+group by u.user_id, u.name
+order by count(mo.movie_id) desc, u.name asc
 limit 1
 )
-
 union all
-
-(   
-select m.title
-from movierating mr
-inner join movies m
-    on mr.movie_id = m.movie_id
-where mr.created_at between '2020-02-01' and '2020-02-29'
-group by mr.movie_id, m.title
-order by avg(mr.rating) desc, m.title asc
+(
+select m.title as results
+from movies m
+inner join movierating mo
+    on m.movie_id = mo.movie_id
+where mo.created_at between '2020-02-01' and '2020-02-29'
+group by mo.movie_id
+order by avg(mo.rating) desc, m.title asc
 limit 1
 )
-
-
-
-
-
-
-
-
-
-
-
 
 
 
