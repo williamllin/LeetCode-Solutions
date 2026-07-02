3d8ca1664e24f0ec790654b625cf43e5b4288c31
William Lin <willyrblin@gmail.com>
2026-07-02 15:47:28 +0800

    Time: 526 ms (6.04%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,12 +1,10 @@
-# Write your MySQL query statement below
-
 SELECT 
     id,
     CASE 
-        -- if odd number then take next id's name
-        -- COALESCE: if next line is NULL, keep it's own name
+        #COALESCE: if next line is NULL, keep it's own name
+        #odd: use LEAD to grab next one
         WHEN id % 2 = 1 THEN COALESCE(LEAD(student, 1) OVER(ORDER BY id), student)
-        -- if even number, take last id's name
+        #even: use LAG to grab previous one
         ELSE LAG(student, 1) OVER(ORDER BY id)
     END AS student
 FROM seat
