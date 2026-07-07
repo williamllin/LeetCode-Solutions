a6355c8a84a5464de38f8fd8a336f8bfcbd4d470
William Lin <willyrblin@gmail.com>
2026-07-07 11:46:27 +0800

    Time: 309 ms (82.6%), Space: 0B (100%) - LeetHub

--- 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -1,29 +1,13 @@
-
-
-
-
-with allfriend as(
+#when appear in either requester or accepter, count as a num, since no 'a to b' and 'b accept a' situation 
+with allfriend as (
     select requester_id as id from requestaccepted
-    union all
+    union all #use union all so that NO 'distinct' function
     select accepter_id as id from requestaccepted
 )
+#make requester, accepter columns into the same column and count it.
 
 select id, count(*) as num
 from allfriend
 group by id
 order by num desc
 limit 1
-
-
-
-
-
-
-
-
-
-
-
-
-
-
