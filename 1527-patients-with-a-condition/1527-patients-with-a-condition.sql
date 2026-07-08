a103782a6a6d81d3a0bd62533d836ebc2bae1fc5
William Lin <willyrblin@gmail.com>
2026-07-08 12:23:07 +0800

    Time: 429 ms (47.21%), Space: 0B (100%) - LeetHub

--- 1527-patients-with-a-condition/1527-patients-with-a-condition.sql
+++ 1527-patients-with-a-condition/1527-patients-with-a-condition.sql
@@ -2,5 +2,3 @@ select patient_id, patient_name, conditions
 from patients
 where conditions like 'DIAB1%'
     or conditions like '% DIAB1%'
-#where conditions like '%DIAB1%' (X), since it doesn't specify the position
-
