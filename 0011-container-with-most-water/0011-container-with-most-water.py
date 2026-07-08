957bb41a69823ed4de7b0c2ac1787a15ab22aab9
William Lin <willyrblin@gmail.com>
2026-07-08 17:20:52 +0800

    Time: 62 ms (30.31%), Space: 29.4 MB (88.25%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -21,20 +21,3 @@ class Solution:
                 right -= 1
                 
         return max_water
-
-        left = 0
-        right = len(height)-1
-        max_water = 0
-
-        while left<right:
-            width = right-left
-            height = min(height[left], height[right])
-            current_water = width*height
-
-            max_water = max(max_water, current_water)
-
-            if height[left] < height[right]:
-                left+=1
-            else:
-                right-=1
-        return max_water
