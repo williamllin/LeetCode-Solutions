ef5aaa8a8ea7faef0e30381b0b37edea0ddcc8e7
William Lin <willyrblin@gmail.com>
2026-07-09 11:41:40 +0800

    Time: 314 ms (25.76%), Space: 67.6 MB (10.26%) - LeetHub

--- 1757-recyclable-and-low-fat-products/1757-recyclable-and-low-fat-products.py
+++ 1757-recyclable-and-low-fat-products/1757-recyclable-and-low-fat-products.py
@@ -4,4 +4,3 @@ def find_products(products: pd.DataFrame) -> pd.DataFrame:
     lowrec = (products['low_fats']=='Y') & (products['recyclable']=='Y')
     result = products[lowrec][['product_id']]
     return result
-
