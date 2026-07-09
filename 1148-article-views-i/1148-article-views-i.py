f76ac3a860bfac8ef21f0876ab8f37567f987899
William Lin <willyrblin@gmail.com>
2026-07-09 12:02:57 +0800

    Time: 269 ms (88.16%), Space: 67.6 MB (14.15%) - LeetHub

--- 1148-article-views-i/1148-article-views-i.py
+++ 1148-article-views-i/1148-article-views-i.py
@@ -1,12 +1,10 @@
 import pandas as pd
 
 def article_views(views: pd.DataFrame) -> pd.DataFrame:
-    df = views[views['author_id']==views['viewer_id']]
-    df = df[['author_id']].drop_duplicates()
-    result = df.sort_values(by='author_id').rename(columns = {'author_id':'id'})
+    
+    df = views[views['author_id'] == views['viewer_id']]#condition
+    df = df[['author_id']].drop_duplicates()#drop duplicates
+    
+    #sort(pick column at same time), and rename
+    result = df.sort_values(by='author_id').rename(columns={'author_id':'id'})
     return result
-
-
-
-
-
