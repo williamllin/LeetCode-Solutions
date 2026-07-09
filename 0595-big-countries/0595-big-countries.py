bb764be1b3fced015e516e463ed74f3e0d36943a
William Lin <willyrblin@gmail.com>
2026-07-09 11:35:51 +0800

    Time: 259 ms (78.72%), Space: 69.1 MB (21.01%) - LeetHub

--- 0595-big-countries/0595-big-countries.py
+++ 0595-big-countries/0595-big-countries.py
@@ -5,3 +5,4 @@ def big_countries(world: pd.DataFrame) -> pd.DataFrame:
     is_big = (world['area']>=3000000) | (world['population']>=25000000)
     result = world[is_big][['name','population','area']]
     return result
+
