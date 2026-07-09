e8479fab3574f96cd7e6c5b69742b2524965a6f1
William Lin <willyrblin@gmail.com>
2026-07-09 12:08:53 +0800

    Time: 254 ms (99.56%), Space: 67.6 MB (29.34%) - LeetHub

--- 1683-invalid-tweets/1683-invalid-tweets.py
+++ 1683-invalid-tweets/1683-invalid-tweets.py
@@ -1,7 +1,5 @@
 import pandas as pd
 
 def invalid_tweets(tweets: pd.DataFrame) -> pd.DataFrame:
-    
-
-    d = tweets[tweets['content'].str.len()>15]
-    return d[['tweet_id']]
+    df = tweets[tweets['content'].str.len() > 15]
+    return df[['tweet_id']]
