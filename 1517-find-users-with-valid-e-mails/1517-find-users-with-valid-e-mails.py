bf0a73ce9752810a546dfccfc5d7accbf01a05c8
William Lin <willyrblin@gmail.com>
2026-07-13 15:20:17 +0800

    Time: 315 ms (29.89%), Space: 67.2 MB (79.68%) - LeetHub

--- 1517-find-users-with-valid-e-mails/1517-find-users-with-valid-e-mails.py
+++ 1517-find-users-with-valid-e-mails/1517-find-users-with-valid-e-mails.py
@@ -4,7 +4,7 @@ def valid_emails(users: pd.DataFrame) -> pd.DataFrame:
     pattern = r'^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode\.com$'
     condition = users['mail'].str.match(pattern, na=False)
     return users[condition]
-
+    
     #or
     #pattern = r'^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode\.com$'
     #con1 = users['mail'].str.match(pattern, na=False)
