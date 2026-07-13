9923a18966feed38695ba81be30ca90b234bf5f7
William Lin <willyrblin@gmail.com>
2026-07-13 15:35:42 +0800

    Time: 275 ms (51.38%), Space: 67 MB (8%) - LeetHub

--- 1527-patients-with-a-condition/1527-patients-with-a-condition.py
+++ 1527-patients-with-a-condition/1527-patients-with-a-condition.py
@@ -3,7 +3,7 @@ import pandas as pd
 def find_patients(patients: pd.DataFrame) -> pd.DataFrame:
 
     pattern = r'^DIAB1|.* DIAB1' #starts with DIAB1 or a char+space then DIAB1
-    con1 = patients['conditions'].str.contains(pattern, na=False)
+    con1 = patients['conditions'].str.contains(pattern, na=False) #contains:if contains inside anywhere #match::match exactly
     df = patients[con1]
     result = df[['patient_id','patient_name','conditions']]
     return result
