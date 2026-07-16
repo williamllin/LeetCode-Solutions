b0b8c6499d9343242c7b977716e831a00dcf4157
William Lin <willyrblin@gmail.com>
2026-07-16 14:25:56 +0800

    Time: 310 ms (63.95%), Space: 68.9 MB (49.91%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.py
+++ 0184-department-highest-salary/0184-department-highest-salary.py
@@ -3,16 +3,18 @@ import pandas as pd
 def department_highest_salary(employee: pd.DataFrame, department: pd.DataFrame) -> pd.DataFrame:
     
 
+    #merge
+    df = employee.merge(department, left_on='departmentId',right_on='id',suffixes=('_emp','_dep'))
 
-
-
-
-    df = employee.merge(department, left_on = 'departmentId', right_on = 'id', suffixes =('_emp','_dep'))
+    #get max salary
     df['max_salary'] = df.groupby('departmentId')['salary'].transform('max')
 
+    #select salaries that are max 
     result = df[df['salary'] == df['max_salary']]
-    result = result[['name_emp','name_dep','salary']]
-    result.columns = ['Employee','Department','Salary'] 
+
+    #result
+    result = result[['name_dep','name_emp','salary']]
+    result.columns = ['Department','Employee','Salary']
     return result
 
 
