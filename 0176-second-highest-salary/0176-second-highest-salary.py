ea1e1a7b6299b99c59ae990878bf066ee71488ce
William Lin <willyrblin@gmail.com>
2026-07-16 12:25:24 +0800

    Time: 285 ms (20.44%), Space: 66.8 MB (90.15%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.py
+++ 0176-second-highest-salary/0176-second-highest-salary.py
@@ -2,7 +2,6 @@ import pandas as pd
 
 def second_highest_salary(employee: pd.DataFrame) -> pd.DataFrame:
     
-
     unique_salaries = employee['salary'].drop_duplicates()
     sorted_salaries = unique_salaries.sort_values(ascending = False)
 
@@ -13,4 +12,3 @@ def second_highest_salary(employee: pd.DataFrame) -> pd.DataFrame:
         ans = sorted_salaries.iloc[1]
 
     return pd.DataFrame({f'SecondHighestSalary': [ans]})
-    
