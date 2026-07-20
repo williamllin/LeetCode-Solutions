6d7d674cb4a56396863d8db87e6825e09a016c94
William Lin <willyrblin@gmail.com>
2026-07-20 11:22:51 +0800

    Time: 264 ms (75.04%), Space: 68.2 MB (51.49%) - LeetHub

--- 0596-classes-with-at-least-5-students/0596-classes-with-at-least-5-students.py
+++ 0596-classes-with-at-least-5-students/0596-classes-with-at-least-5-students.py
@@ -5,4 +5,3 @@ def find_classes(courses: pd.DataFrame) -> pd.DataFrame:
     class_count = df.groupby('class', as_index = False)['student'].count() #count students in each class
     result = class_count[class_count['student']>=5]
     return result[['class']]
-    
