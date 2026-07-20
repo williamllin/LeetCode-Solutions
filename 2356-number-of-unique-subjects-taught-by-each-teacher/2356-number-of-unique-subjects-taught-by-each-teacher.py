b92e6ed211b490905ab5677ff798f7fee2ca4f6e
William Lin <willyrblin@gmail.com>
2026-07-20 11:20:02 +0800

    Time: 265 ms (93.31%), Space: 68.2 MB (32.24%) - LeetHub

--- 2356-number-of-unique-subjects-taught-by-each-teacher/2356-number-of-unique-subjects-taught-by-each-teacher.py
+++ 2356-number-of-unique-subjects-taught-by-each-teacher/2356-number-of-unique-subjects-taught-by-each-teacher.py
@@ -2,5 +2,6 @@ import pandas as pd
 
 def count_unique_subjects(teacher: pd.DataFrame) -> pd.DataFrame:
     result = teacher.groupby('teacher_id', as_index=False)['subject_id'].nunique()
+    #teacher 1 teaches sub 2 twice, but we count as 1 so use nunique()
     result.rename(columns={'subject_id':'cnt'}, inplace=True)
     return result
