c9972305f475708fb4091237a9ce92ffdb3614ad
William Lin <willyrblin@gmail.com>
2026-07-20 15:47:35 +0800

    Time: 51 ms (85.45%), Space: 22.1 MB (85.06%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
@@ -3,12 +3,12 @@ class Solution:
         left = 0
         zero_count = 0
         max_len = 0
-
-        for right in range(len(nums)):
-            if nums[right]==0:
+        #for loop default 'right' as 0
+        for right in range(len(nums)): #right pointer moving towards right
+            if nums[right]==0: #if new tail is 0, zero count add 1
                 zero_count+=1
 
-            while zero_count > k:
+            while zero_count > k: #if exceed k, left pointer starts moving right, and if new left is 0, count minus 1
                 if nums[left]==0:
                     zero_count-=1
                 left+=1
