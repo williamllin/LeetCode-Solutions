4e55f8f3b7de1df2c18efdb4b17e2cea5dc27bd3
William Lin <willyrblin@gmail.com>
2026-07-20 11:25:35 +0800

    Time: 277 ms (78.21%), Space: 67.5 MB (21.74%) - LeetHub

--- 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.py
+++ 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.py
@@ -1,7 +1,9 @@
 import pandas as pd
 
 def largest_orders(orders: pd.DataFrame) -> pd.DataFrame:
-    df_count = orders.groupby('customer_number', as_index = False)['order_number'].count()
-    df_sort = df_count.sort_values(by='order_number', ascending = False)
+    
+
+    df = orders.groupby('customer_number',as_index=False)['order_number'].count()
+    df_sort = df.sort_values(by='order_number', ascending = False)
     result = df_sort.head(1)[['customer_number']]
     return result
