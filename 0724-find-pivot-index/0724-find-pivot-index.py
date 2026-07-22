3525fb096fc9505e00b2e08667ea6bde7c6df54d
William Lin <willyrblin@gmail.com>
2026-07-22 16:12:10 +0800

    Time: 5 ms (63.69%), Space: 20.4 MB (45.8%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.py
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -8,5 +8,21 @@ class Solution:
                 return i #return the 'index'
 
             left_sum += x
-            
+
         return -1
+'''
+index(0) 0 == 28 -0 -1 =27(X)
+index(1) 1 == 28 -1 -7 =20(X)
+index(2) 8 == 28 -8 -3 =17(X)
+index(3) 11 == 28 -11 -6 =11(O)
+
+
+        
+        for i in range(len(nums)):
+            if left_sum == total_sum - left_sum - nums[i]:
+                return i
+
+            left_sum += nums[i]
+
+        return -1
+        '''
