b624dedf16a2857a1aa958a879482f8236ea6b24
William Lin <willyrblin@gmail.com>
2026-07-23 12:45:57 +0800

    Time: 270 ms (87.85%), Space: 69.5 MB (12.32%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.py
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.py
@@ -1,15 +1,16 @@
 import pandas as pd
 
 def find_managers(employee: pd.DataFrame) -> pd.DataFrame:
-    merge_df = employee.merge(
+    
+    
+    df = employee.merge(
         employee,
         left_on = 'id',
         right_on = 'managerId',
         suffixes=('_mgr','_emp')
-    )#101 links to all 'managerId 101'
+    )
 
-    group = merge_df.groupby('id_mgr').size().reset_index(name='report_count') #count the number of mrg_id
+    group = df.groupby('id_mgr').size().reset_index(name='report_count')
     valid_managers = group[group['report_count']>=5]
     result = employee[employee['id'].isin(valid_managers['id_mgr'])][['name']]
-
     return result
