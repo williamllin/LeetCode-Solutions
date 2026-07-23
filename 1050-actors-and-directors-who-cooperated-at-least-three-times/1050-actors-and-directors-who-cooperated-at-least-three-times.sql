7bfe65571780b9dca86ab1167be6950cef92bdd0
William Lin <willyrblin@gmail.com>
2026-07-23 12:08:46 +0800

    Time: 382 ms (49.64%), Space: 0B (100%) - LeetHub

--- 1050-actors-and-directors-who-cooperated-at-least-three-times/1050-actors-and-directors-who-cooperated-at-least-three-times.sql
+++ 1050-actors-and-directors-who-cooperated-at-least-three-times/1050-actors-and-directors-who-cooperated-at-least-three-times.sql
@@ -1,10 +1,9 @@
-# Write your MySQL query statement below
 select actor_id, director_id
 from actordirector
-where (actor_id, director_id) in (
+where(actor_id, director_id) in (
     select actor_id, director_id
     from actordirector
     group by actor_id, director_id
-    having count(*) >= 3
+    having count(*)>=3
 )
 group by actor_id, director_id
