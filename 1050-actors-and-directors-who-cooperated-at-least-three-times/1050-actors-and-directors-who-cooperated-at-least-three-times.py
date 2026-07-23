44a29878ee8a5505657b38c092b7904194833107
William Lin <willyrblin@gmail.com>
2026-07-23 12:12:20 +0800

    Time: 270 ms (86.25%), Space: 67.9 MB (37.98%) - LeetHub

--- 1050-actors-and-directors-who-cooperated-at-least-three-times/1050-actors-and-directors-who-cooperated-at-least-three-times.py
+++ 1050-actors-and-directors-who-cooperated-at-least-three-times/1050-actors-and-directors-who-cooperated-at-least-three-times.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 def actors_and_directors(actor_director: pd.DataFrame) -> pd.DataFrame:
-    df_count = actor_director.groupby(['actor_id', 'director_id']).size().reset_index(name='co-op_times')
-    df_res = df_count[df_count['co-op_times'] >=3 ]
+    df_count = actor_director.groupby(['actor_id','director_id']).size().reset_index(name='co-op_times')
+    df_res = df_count[df_count['co-op_times']>=3]
+    
     return df_res[['actor_id','director_id']]
