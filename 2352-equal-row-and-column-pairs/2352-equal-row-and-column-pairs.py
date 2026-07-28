cae829bb53f5d61482b43ccc7837b0f73143f146
William Lin <willyrblin@gmail.com>
2026-07-28 16:45:33 +0800

    Time: 31 ms (42.33%), Space: 24.6 MB (55%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
@@ -7,13 +7,13 @@ class Solution:
         n = len(grid)
         #list is mutable so can't use as key of dictionary or counter
         #tuple is immutable and can use as key of hash table
-        row_counts = Counter(tuple(row) for row in grid)
+        row_counts = Counter(tuple(row) for row in grid) #{(3, 1, 2, 2): 1, (2, 4, 2, 2): 2...}
 
         ans = 0
 
         #take out each column and compare
         for c in range(n):#c column
-            col = tuple(grid[r][c] for r in range(n))#if col in row_counts, add 
-            ans += row_counts[col] 
+            col = tuple(grid[r][c] for r in range(n))
+            ans += row_counts[col] #if column matches, add
 
         return ans
