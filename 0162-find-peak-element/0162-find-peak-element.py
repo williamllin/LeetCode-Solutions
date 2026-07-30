1a12a39d4ab6b86de2339d8f30a8fa5228e379a6
William Lin <willyrblin@gmail.com>
2026-07-30 12:18:29 +0800

    Time: 0 ms (100%), Space: 19.1 MB (96.44%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.py
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -1,18 +1,17 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
 
-        left, right = 0, len(nums) - 1
         #binary search for local peak -> O(log n)
+        left, right = 0, len(nums) - 1
         while left < right:
             mid = (left + right) // 2
             
             #compare mid element with its right neighbor
             if nums[mid] < nums[mid + 1]:
-                left = mid + 1 #on an incline, peak must be on the right
+                left = mid + 1 #incline, peak on the right
             else:
-                right = mid #on a decline, peak is mid or to the left
+                right = mid #decline, peak is mid or to the left
 
-   
         #when left == right, we have converged on a peak index
         return left
 
