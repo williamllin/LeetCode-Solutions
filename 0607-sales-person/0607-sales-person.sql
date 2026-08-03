95c2045972322fa19e802513d222e4e90b930be7
William Lin <willyrblin@gmail.com>
2026-08-03 17:00:44 +0800

    Time: 1871 ms (12.14%), Space: 0B (100%) - LeetHub

--- 0607-sales-person/0607-sales-person.sql
+++ 0607-sales-person/0607-sales-person.sql
@@ -1,10 +1,11 @@
-# Write your MySQL query statement below
-select name
-from salesperson
-where sales_id not in ( #where xxx IN/NOT IN, if where xxx is one column, the subquery should be one column as well
+
+select s.name
+from salesperson s
+where s.sales_id not in (
     select o.sales_id
     from orders o
     join company c
         on o.com_id = c.com_id
-    where c.name = 'RED'
+    where c.name ='RED'
 )
+#where xxx IN/NOT IN, if where xxx is one column, the subquery should be one column as well
