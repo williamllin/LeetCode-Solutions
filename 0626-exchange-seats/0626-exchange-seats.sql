454adc0ae7348dbc5c150f1ec40f12c42da3abf8
William Lin <willyrblin@gmail.com>
2026-08-04 12:10:03 +0800

    Time: 448 ms (12.41%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,3 +1,4 @@
+/*
 SELECT 
     id,
     CASE 
@@ -8,3 +9,17 @@ SELECT
         ELSE LAG(student, 1) OVER(ORDER BY id)
     END AS student
 FROM seat
+*/
+
+select id,
+    case 
+        when id % 2 = 1 then coalesce(lead(student, 1) over(order by id), student) 
+        else lag(student, 1) over(order by id)
+    end as student
+from seat
+
+/*
+lag: take previous
+lead: take next
+coalesce: if NULL, use original(ex: no id6, so use Jeames)
+*/
