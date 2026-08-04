aae6480c862e1308e39c8001757359ac2067cb10
William Lin <willyrblin@gmail.com>
2026-08-04 12:37:52 +0800

    Time: 621 ms (41.8%), Space: 0B (100%) - LeetHub

--- 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
+++ 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
@@ -1,5 +1,11 @@
+/*
 select customer_id
 from customer
 group by customer_id
 having count(distinct product_key) = (select count(*) from product)
+*/
 
+select customer_id
+from customer
+group by customer_id
+having count(distinct product_key) = (select count(*) from product)
