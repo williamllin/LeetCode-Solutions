52e15f246ee00f67685546e8804e247154f41ddb
William Lin <willyrblin@gmail.com>
2026-08-04 12:29:21 +0800

    Time: 1665 ms (32.58%), Space: 0B (100%) - LeetHub

--- 1907-count-salary-categories/1907-count-salary-categories.sql
+++ 1907-count-salary-categories/1907-count-salary-categories.sql
@@ -1,19 +1,20 @@
 #union: remove duplicates, union all: do not
+
 select 
-    'Low Salary' as category,
+    'Low Salary' as category, 
     sum(case when income <20000 then 1 else 0 end) as accounts_count
 from accounts
 
 union all
 
-select
-    'Average Salary' as category,
-    sum(case when income>=20000 and income<=50000 then 1 else 0 end) as accounts_count
+select 
+    'Average Salary' as category, 
+    sum(case when income >=20000 and income <= 50000 then 1 else 0 end) as accounts_count
 from accounts
 
 union all
 
-select
-    'High Salary' as category,
-    sum(case when income>50000 then 1 else 0 end) as accounts_count
+select 
+    'High Salary' as category, 
+    sum(case when income >50000 then 1 else 0 end) as accounts_count
 from accounts
