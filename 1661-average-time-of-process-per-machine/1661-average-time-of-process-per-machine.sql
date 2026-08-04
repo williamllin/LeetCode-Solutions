464454bf0e3f4b4a77f1dcc4aedc3608bd6b25e1
William Lin <willyrblin@gmail.com>
2026-08-04 12:02:48 +0800

    Time: 310 ms (13.96%), Space: 0B (100%) - LeetHub

--- 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
+++ 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -1,3 +1,4 @@
+/*
 select a1.machine_id, round(avg(a1.timestamp - a2.timestamp),3) as processing_time
 from activity a1
     join activity a2
@@ -6,6 +7,28 @@ from activity a1
 where a1.activity_type like 'end'
     and a2.activity_type like 'start'
 group by a1.machine_id #
+*/
+select a1.machine_id, round(avg(a1.timestamp - a2.timestamp),3) as processing_time
+from activity a1
+    join activity a2
+        on a1.machine_id = a2.machine_id
+        and a1.process_id = a2.process_id
+where a1.activity_type like 'end'
+    and a2.activity_type like 'start'
+group by a1.machine_id
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
