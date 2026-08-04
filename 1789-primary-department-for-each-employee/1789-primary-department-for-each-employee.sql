4d4acdd1a72590f26f873a6fb994bee098014014
William Lin <willyrblin@gmail.com>
2026-08-04 12:42:09 +0800

    Time: 636 ms (28.58%), Space: 0B (100%) - LeetHub

--- 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -1,5 +1,7 @@
+/*
 select employee_id, department_id
 from employee
+
 where primary_flag = 'Y'
     or employee_id in (
         select employee_id
@@ -7,4 +9,13 @@ where primary_flag = 'Y'
         group by employee_id
         having count(department_id)=1
     )
-
+*/
+select employee_id, department_id
+from employee
+where primary_flag like 'Y'
+    or employee_id in (
+        select employee_id
+        from employee
+        group by employee_id
+        having count(primary_flag)=1
+    )
