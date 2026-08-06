9061a31ddb8f1f195c4d5d3a24600e19c960258a
William Lin <willyrblin@gmail.com>
2026-08-06 16:35:51 +0800

    Time: 618 ms (48.67%), Space: 0B (100%) - LeetHub

--- 0585-investments-in-2016/0585-investments-in-2016.sql
+++ 0585-investments-in-2016/0585-investments-in-2016.sql
@@ -1,5 +1,6 @@
 #con1: more than one same 2015 investment
 #con2: different cities
+/*
 select round(sum(tiv_2016),2) as tiv_2016
 from insurance
 where tiv_2015 in (
@@ -14,8 +15,20 @@ where tiv_2015 in (
         group by lat, lon
         having count(*) = 1
     )
-
-
+*/
+select round(sum(tiv_2016),2) as tiv_2016
+from insurance
+where tiv_2015 in (
+    select tiv_2015
+    from insurance
+    group by tiv_2015
+    having count(*) >=2
+) and (lat, lon) in (
+    select lat, lon
+    from insurance
+    group by lat, lon
+    having count(*) =1
+)
 
 
 
