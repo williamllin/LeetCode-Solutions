83999702bec6bdab4f2eb513b29c9cfdff631427
William Lin <willyrblin@gmail.com>
2026-08-10 12:35:47 +0800

    Time: 327 ms (23.48%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -9,12 +9,18 @@ select max(salary) as SecondHighestSalary
 from rankedemployee
 where salary_rank = 2
 */
+
+
+
+
+
 with rankedsalary as(
     select id, salary,
         dense_rank()over(order by salary desc) as salary_ranked
     from employee
 )
-
-select max(salary) as SecondHighestSalary
+#use max() so that if no 2ng high, return NULL
+#since agg: if nothing and no group by, will return null
+select max(salary) as SecondHighestSalary 
 from rankedsalary
 where salary_ranked = 2
