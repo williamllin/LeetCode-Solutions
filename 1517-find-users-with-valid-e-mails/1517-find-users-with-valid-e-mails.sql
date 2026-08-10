45198aec116d9f8a830c2156d12413e2bc4e7d73
William Lin <willyrblin@gmail.com>
2026-08-10 12:51:57 +0800

    Time: 1075 ms (10.7%), Space: 0B (100%) - LeetHub

--- 1517-find-users-with-valid-e-mails/1517-find-users-with-valid-e-mails.sql
+++ 1517-find-users-with-valid-e-mails/1517-find-users-with-valid-e-mails.sql
@@ -4,7 +4,11 @@ select user_id, name, mail
 from users
 where mail regexp '^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode\\.com$'
     and mail like binary '%@leetcode.com' #binary: case sensitive
+
+
+    
     # ^, $ : start, end of string
     # [a-zA-Z] : first word
     # [a-zA-Z0-9_.-]* : words following the first, * means can be any length
     # \\. : since '.' has special meaning so add \\ to transform back to normal '.'
+
