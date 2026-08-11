d9ac9713ee959af58e7a2b5a76cddfff4c35fe3f
William Lin <willyrblin@gmail.com>
2026-08-11 16:28:17 +0800

    Time: 335 ms (43.24%), Space: 67.4 MB (61.95%) - LeetHub

--- 1795-rearrange-products-table/1795-rearrange-products-table.py
+++ 1795-rearrange-products-table/1795-rearrange-products-table.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 def rearrange_products_table(products: pd.DataFrame) -> pd.DataFrame:
+    '''
     result = pd.melt(
         products, 
         id_vars=['product_id'], 
@@ -10,5 +11,15 @@ def rearrange_products_table(products: pd.DataFrame) -> pd.DataFrame:
     )
     result = result.dropna(subset=['price'])
     
+    return result
+    '''
+    result = pd.melt(
+        products,
+        id_vars=['product_id'],
+        value_vars=['store1','store2','store3'],
+        var_name='store',
+        value_name='price'
+    )
+    result = result.dropna(subset=['price'])
     return result
     
