18675efb3863da86c020fc5c8047c7f9e3cc74e5
William Lin <willyrblin@gmail.com>
2026-08-11 11:50:56 +0800

    Time: 287 ms (95.25%), Space: 69 MB (32.71%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.py
+++ 0184-department-highest-salary/0184-department-highest-salary.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 def department_highest_salary(employee: pd.DataFrame, department: pd.DataFrame) -> pd.DataFrame:
+    '''
     #merge, max_sal in department, select max_sal rows for result
     df = employee.merge(department, left_on='departmentId', right_on='id', suffixes=('_emp','_dep'))
     df['max_salary'] = df.groupby('departmentId')['salary'].transform('max')
@@ -12,18 +13,15 @@ def department_highest_salary(employee: pd.DataFrame, department: pd.DataFrame) 
     return result
     
     '''
-    import pandas as pd
 
+    import  pandas as pd
     df = employee.merge(department, left_on='departmentId', right_on='id', suffixes=('_emp','_dep'))
     df['max_salary'] = df.groupby('departmentId')['salary'].transform('max')
     result = df[df['salary']==df['max_salary']]
 
-    result = result['name_dep','name_emp','salary']
-    result.columns=['Department','Employee','Salary']
+    result = result[['name_dep','name_emp','salary']]
+    result.columns = ['Department','Employee','Salary']
     return result
-    '''
-
-
 
 
 
