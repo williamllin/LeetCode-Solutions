7451bf2233419e8e684f50b9ac8725275f89fdf0
William Lin <willyrblin@gmail.com>
2026-08-11 13:01:21 +0800

    Time: 303 ms (17.41%), Space: 68.3 MB (37.48%) - LeetHub

--- 0178-rank-scores/0178-rank-scores.py
+++ 0178-rank-scores/0178-rank-scores.py
@@ -1,12 +1,20 @@
 import pandas as pd
 
 def order_scores(scores: pd.DataFrame) -> pd.DataFrame:
-    
-    #if empty, return empty
+    '''
+    #if empty, return empty.
     if scores.empty:
         return pd.DataFrame(columns=['score','rank'])
 
     scores['rank'] = scores['score'].rank(method='dense',ascending=False)#dense:same score get same rank
 
     result = scores.sort_values(by='score',ascending=False)[['score','rank']]
+    return result
+    '''
+    if scores.empty:
+        return pd.DataFrame(columns=['score','rank'])
+    
+    scores['rank'] = scores['score'].rank(method='dense', ascending=False)#dense: same score, same rank
+
+    result = scores.sort_values(by='score', ascending=False)[['score','rank']]
     return result
