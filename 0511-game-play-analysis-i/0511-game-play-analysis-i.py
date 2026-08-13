7d959bcc245a8a6428b02d9b04f92bed16d253ff
William Lin <willyrblin@gmail.com>
2026-08-13 11:49:59 +0800

    Time: 288 ms (74.43%), Space: 68.3 MB (31.62%) - LeetHub

--- 0511-game-play-analysis-i/0511-game-play-analysis-i.py
+++ 0511-game-play-analysis-i/0511-game-play-analysis-i.py
@@ -1,7 +1,14 @@
 import pandas as pd
 
 def game_analysis(activity: pd.DataFrame) -> pd.DataFrame:
+    '''
     activity['event_date'] = pd.to_datetime(activity['event_date']) #string to date
     result = activity.groupby('player_id', as_index=False)['event_date'].min() ###
     result.rename(columns={'event_date': 'first_login'}, inplace=True)
+    return result
+    '''
+
+    activity['event_date'] = pd.to_datetime(activity['event_date'])
+    result = activity.groupby('player_id', as_index=False)['event_date'].min()
+    result.rename(columns={'event_date':'first_login'}, inplace=True)
     return result
