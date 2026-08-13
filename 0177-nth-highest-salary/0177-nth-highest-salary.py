b583ff19da30714c2831a97f9c065420c42ffd62
William Lin <willyrblin@gmail.com>
2026-08-13 16:31:56 +0800

    Time: 294 ms (26.98%), Space: 67.1 MB (50.4%) - LeetHub

--- 0177-nth-highest-salary/0177-nth-highest-salary.py
+++ 0177-nth-highest-salary/0177-nth-highest-salary.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 def nth_highest_salary(employee: pd.DataFrame, N: int) -> pd.DataFrame:
+    '''
     if N<=0:
         return pd.DataFrame({f'getNthHighestSalary({N})': [None]})
     
@@ -13,4 +14,15 @@ def nth_highest_salary(employee: pd.DataFrame, N: int) -> pd.DataFrame:
         ans = sorted_salaries.iloc[N-1]
 
     return pd.DataFrame({f'getNthHighestSalary({N})': [ans]})
-        
+    '''
+    if N<=0:
+        return pd.DataFrame({f'getNthHighestSalary({N})': [None]})
+    
+    unique_salary = employee['salary'].drop_duplicates()
+    sorted_salary = unique_salary.sort_values(ascending=False)
+
+    if N>len(sorted_salary):
+        ans = None
+    else:
+        ans = sorted_salary.iloc[N-1]
+    return pd.DataFrame({f'getNthHighestSalary({N})': [ans]})
