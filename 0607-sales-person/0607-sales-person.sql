584a6652cbdd0a38c0bf2b9f8a01d66be6bc00c0
William Lin <willyrblin@gmail.com>
2026-08-22 23:01:20 +0800

    Time: 1361 ms (87.96%), Space: 0B (100%) - LeetHub

--- 0607-sales-person/0607-sales-person.sql
+++ 0607-sales-person/0607-sales-person.sql
@@ -1,4 +1,4 @@
-
+/*
 select s.name
 from salesperson s
 where s.sales_id not in (
@@ -9,3 +9,27 @@ where s.sales_id not in (
     where c.name ='RED'
 )
 #where xxx IN/NOT IN, if where xxx is one column, the subquery should be one column as well
+
+*/
+select s.name as name
+from salesperson s
+where s.sales_id not in (
+    select o.sales_id
+    from orders o
+    join company c
+        on o.com_id = c.com_id
+    where c.name like 'RED'
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
