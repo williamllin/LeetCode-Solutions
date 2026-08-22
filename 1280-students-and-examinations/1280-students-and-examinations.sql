91ee27a1e13f0353a78c4da372f241cf2261bf64
William Lin <willyrblin@gmail.com>
2026-08-22 22:55:40 +0800

    Time: 1051 ms (65.25%), Space: 0B (100%) - LeetHub

--- 1280-students-and-examinations/1280-students-and-examinations.sql
+++ 1280-students-and-examinations/1280-students-and-examinations.sql
@@ -2,7 +2,7 @@
 #count(e.subject_name) so that there's 'NULL' to be displayed as '0'
 #cross join makes every students match each subject, doesn't matter if they took it or not
 
-
+/*
 select st.student_id, st.student_name, su.subject_name, count(e.subject_name) as attended_exams
 from students st
     cross join subjects su
@@ -12,6 +12,28 @@ from students st
 
 group by st.student_id, st.student_name, su.subject_name
 order by st.student_id, su.subject_name
+*/
+
+
+select s.student_id, s.student_name, su.subject_name, count(e.subject_name) as attended_exams
+from students s
+cross join subjects su
+    left join examinations e
+        on s.student_id = e.student_id
+        and su.subject_name = e.subject_name
+group by s.student_id, s.student_name, su.subject_name
+order by s.student_id, su.subject_name
+
+
+
+
+
+
+
+
+
+
+
 
 
 
