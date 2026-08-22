b39461bd87f583b477c2a54634465cbc36a2601a
William Lin <willyrblin@gmail.com>
2026-08-22 23:18:47 +0800

    Time: 780 ms (71.23%), Space: 0B (100%) - LeetHub

--- 1517-find-users-with-valid-e-mails/1517-find-users-with-valid-e-mails.sql
+++ 1517-find-users-with-valid-e-mails/1517-find-users-with-valid-e-mails.sql
@@ -1,14 +1,19 @@
 #too many conditions, so using where xxx LIKE and xxx NOT LIKE.... won't work
 #regular expression
+/*
 select user_id, name, mail
 from users
 where mail regexp '^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode\\.com$'
     and mail like binary '%@leetcode.com' #binary: case sensitive
 
-
+*/
     
     # ^, $ : start, end of string
     # [a-zA-Z] : first word
     # [a-zA-Z0-9_.-]* : words following the first, * means can be any length
     # \\. : since '.' has special meaning so add \\ to transform back to normal '.'
 
+select user_id, name, mail
+from users
+where mail regexp '^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode\\.com$'
+    and mail like binary '%@leetcode.com'
