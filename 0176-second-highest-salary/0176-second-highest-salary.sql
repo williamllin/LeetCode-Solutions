1970d7529d23d4c91db41ccd569a02a281f47ccf
William Lin <willyrblin@gmail.com>
2026-08-22 22:43:56 +0800

    Time: 308 ms (43.33%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,19 +1,4 @@
 /*
-with rankedemployee as (
-    select salary,
-    dense_rank()over(order by salary desc) as salary_rank
-    #rank through salary and create column:salary_rank
-    from employee
-)
-select max(salary) as SecondHighestSalary
-from rankedemployee
-where salary_rank = 2
-*/
-
-
-
-
-
 with rankedsalary as(
     select id, salary,
         dense_rank()over(order by salary desc) as salary_ranked
@@ -24,3 +9,21 @@ with rankedsalary as(
 select max(salary) as SecondHighestSalary 
 from rankedsalary
 where salary_ranked = 2
+*/
+
+with ranked_salary as(
+    select id, salary
+        , dense_rank()over(order by salary desc) as salary_rank
+    from employee
+)
+select max(salary) as SecondHighestSalary
+from ranked_salary
+where salary_rank = 2
+
+
+
+
+
+
+
+
