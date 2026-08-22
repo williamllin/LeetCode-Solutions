eeab2f611489f634ba2ed43625d4123cb0d44e22
William Lin <willyrblin@gmail.com>
2026-08-22 23:27:06 +0800

    Time: 1802 ms (17.99%), Space: 0B (100%) - LeetHub

--- 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -19,9 +19,29 @@ vid/cusid/tid
 7   54    null
 8   54    null
 */
+/*
 select v.customer_id, count(v.customer_id) as count_no_trans
 from visits v
     left join transactions t 
+        on v.visit_id = t.visit_id
+where t.transaction_id is null
+group by v.customer_id
+
+
+v  c    t
+1  23   12
+2  9    13
+4  30
+5  54   2
+5  54   3
+5  54   9
+6  96
+7  54
+8  54
+*/
+select v.customer_id, count(v.customer_id) as count_no_trans
+from visits v
+    left join transactions t
         on v.visit_id = t.visit_id
 where t.transaction_id is null
 group by v.customer_id
