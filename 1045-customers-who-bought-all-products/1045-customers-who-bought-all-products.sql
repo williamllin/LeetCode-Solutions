24c88ff3e7b23de165cd9f346ed29665b5cbd4c6
William Lin <willyrblin@gmail.com>
2026-08-23 23:43:10 +0800

    Time: 593 ms (67.89%), Space: 0B (100%) - LeetHub

--- 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
+++ 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
@@ -4,14 +4,8 @@ from customer
 group by customer_id
 having count(distinct product_key) = (select count(*) from product)
 */
-/*
 select customer_id
 from customer
 group by customer_id
 having count(distinct product_key) = (select count(*) from product)
-*/
-
-select customer_id
-from customer
-group by customer_id
-having count(distinct product_key) = (select count(*) from product)
+    #Prodduct table doesn't need distinct cause it doesn't duplicate
