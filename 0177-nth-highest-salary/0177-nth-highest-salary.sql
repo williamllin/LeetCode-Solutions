c686c9a57f29624d07742f2135308ec27cc01dcc
William Lin <willyrblin@gmail.com>
2026-08-23 23:18:07 +0800

    Time: 460 ms (70.55%), Space: 0B (100%) - LeetHub

--- 0177-nth-highest-salary/0177-nth-highest-salary.sql
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -1,4 +1,5 @@
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+/*
 BEGIN
   declare M int; #use M to store the offset value
   set M = N-1;
@@ -9,3 +10,14 @@ BEGIN
     limit 1 offset M
   );
 END
+*/
+begin
+declare m int;
+set m = N-1;
+return(
+    select distinct salary
+    from employee
+    order by salary desc
+    limit 1 offset m
+);
+end
