e0f089f8a518dbbcfc65629e194f72e6e8b87006
William Lin <willyrblin@gmail.com>
2026-08-23 23:51:55 +0800

    Time: 727 ms (19.78%), Space: 0B (100%) - LeetHub

--- 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -13,9 +13,11 @@ where primary_flag = 'Y'
 select employee_id, department_id
 from employee
 where primary_flag like 'Y'
-    or employee_id in (
+    or employee_id in(
         select employee_id
         from employee
         group by employee_id
         having count(primary_flag)=1
     )
+#"LIKE" operator is used for string pattern matching with wildcards
+#"IS" operator is used specifically to evaluate NULL states
