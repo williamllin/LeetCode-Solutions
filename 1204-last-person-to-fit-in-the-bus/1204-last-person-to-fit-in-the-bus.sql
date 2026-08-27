bf2c9aac456ff28862d2bcc70affb1f34f107a35
William Lin <willyrblin@gmail.com>
2026-08-27 10:53:46 +0800

    Time: 806 ms (82.23%), Space: 0B (100%) - LeetHub

--- 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
@@ -10,17 +10,13 @@ where total_weight <= 1000
 order by turn desc
 limit 1
 */
-
-with cumulativetable as(
-    select person_name, turn,
-        sum(weight)over(order by turn) as total_weight
+with cumulativetable as (
+    select person_name, turn
+        ,sum(weight) over(order by turn) as total_weight
     from queue
 )
-
 select person_name
 from cumulativetable
 where total_weight <= 1000
 order by turn desc
 limit 1
-
-
