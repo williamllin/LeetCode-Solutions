1d206f186151cbbc7b7a902fc2b7ce3174cd783c
William Lin <willyrblin@gmail.com>
2026-08-27 10:30:08 +0800

    Time: 533 ms (92.87%), Space: 0B (100%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -5,17 +5,17 @@ with preparedtable as (
         lead(num, 2)over(order by id) as nextnextnum
     from logs
 )
+
 select distinct num as ConsecutiveNums
 from preparedtable
 where num = nextnum and num = nextnextnum
 */
-with scantable as(
+with preparedtable as(
     select num,
-        lead(num,1)over(order by id) as next_num,
-        lead(num,2)over(order by id) as nextnext_num
+        lead(num,1)over(order by id) as nextnum,
+        lead(num,2)over(order by id) as nextnextnum
     from logs
 )
-
 select distinct num as ConsecutiveNums
-from scantable
-where num = next_num and next_num = nextnext_num
+from preparedtable
+where num = nextnum and num = nextnextnum
