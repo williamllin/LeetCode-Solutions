ab50a388c1b32cef67a8a6f950ec1bebd2761744
William Lin <willyrblin@gmail.com>
2026-08-27 10:48:03 +0800

    Time: 476 ms (81.89%), Space: 0B (100%) - LeetHub

--- 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -19,7 +19,7 @@ having min(change_date) >'2019-08-16'
 #available date before 8/16
 select product_id, new_price as price
 from products
-where (product_id, change_date) in(
+where (product_id, change_date) in (
     select product_id, max(change_date)
     from products
     where change_date <= '2019-08-16'
