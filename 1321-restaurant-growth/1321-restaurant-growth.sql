6b1f7bf0d6d5875233752f1c5d5c92818d8ffa3f
William Lin <willyrblin@gmail.com>
2026-08-27 16:41:58 -0500

    Time: 326 ms (98.07%), Space: 0B (100%) - LeetHub

--- 1321-restaurant-growth/1321-restaurant-growth.sql
+++ 1321-restaurant-growth/1321-restaurant-growth.sql
@@ -17,35 +17,19 @@ from movingmetrics
 where datediff(visited_on, (select min(visited_on) from movingmetrics)) >=6
 order by visited_on asc
 */
-
-
-with dailyamount as (
+with dailyamount as(
     select visited_on, sum(amount) as daily_amount
     from customer
     group by visited_on
-), #','
+),
 
-movingmetrics as ( #don't need another 'with'
-    select 
-        visited_on, 
-        sum(daily_amount)over(order by visited_on rows 6 preceding) as amount,
+movingmetrics as(
+    select visited_on,
+        sum(daily_amount)over(order by visited_on rows 6 preceding) as amount,#sum every 6 days
         round(avg(daily_amount)over(order by visited_on rows 6 preceding),2) as average_amount
     from dailyamount
 )
 select visited_on, amount, average_amount
 from movingmetrics
-where datediff(visited_on, (select min(visited_on) from movingmetrics)) >=6
+where datediff (visited_on, (select min(visited_on) from movingmetrics)) >=6
 order by visited_on asc
-
-
-
-
-
-
-
-
-
-
-
-
-
