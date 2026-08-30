a9d3fc548468077274a55d3db5232eed8b8b2f1d
William Lin <willyrblin@gmail.com>
2026-08-30 14:22:52 -0500

    Time: 483 ms (61.57%), Space: 0B (100%) - LeetHub

--- 1484-group-sold-products-by-the-date/1484-group-sold-products-by-the-date.sql
+++ 1484-group-sold-products-by-the-date/1484-group-sold-products-by-the-date.sql
@@ -1,3 +1,11 @@
+/*
+select sell_date, count(distinct product) as num_sold,
+    group_concat(distinct product order by product separator ',') as products
+from activities
+group by sell_date
+order by sell_date asc
+*/
+
 select sell_date, count(distinct product) as num_sold,
     group_concat(distinct product order by product separator ',') as products
 from activities
