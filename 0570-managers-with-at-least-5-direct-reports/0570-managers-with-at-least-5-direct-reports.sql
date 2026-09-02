9425c14cb40c10c836b2056b015601010886b030
William Lin <willyrblin@gmail.com>
2026-09-01 22:22:50 -0500

    Time: 354 ms (76.07%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,47 +1,19 @@
-# Write your MySQL query statement below
-
 /*
-SELECT name 
-FROM Employee 
-WHERE id IN (
-    SELECT managerId 
-    FROM Employee 
-    GROUP BY managerId 
-    HAVING COUNT(id) >= 5
+select e.name as name
+from employee e
+where e.id in (
+    select managerId
+    from employee
+    group by managerId
+    having count(id)>=5
 )
 */
-
 select ma.name
 from employee e
-    join (select id, name #inner join to void manager id that doesn't exist in this company(ex:100)
-        from employee e) ma
-            on e.managerId = ma.id
-group by ma.name, ma.id #group by name since name is the main, group by id since people might have same name
-having count(e.id) >=5 
-#use employee id to count instead manager id, avoid null employee id(some managers might not have employees to match)
-
-/*
-table after join:
-e.id   e.name   e.dep   e.magId   ma.name   ma.id
-102    dan      a       101       john      101
-103    james    a       101       john      101
-104    amy      a       101       john      101
-105    anne     a       101       john      101
-106    ron      b       101       john      101
-*/
-
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
+    join (
+        select id, name
+        from employee e
+    ) ma 
+        on e.managerId = ma.id
+group by ma.id, ma.name
+having count(e.id)>=5
