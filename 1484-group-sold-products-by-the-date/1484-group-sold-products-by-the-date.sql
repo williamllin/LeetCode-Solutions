8ae051af7d70e229536642707910a9ce92b995d9
William Lin <willyrblin@gmail.com>
2026-09-01 21:59:19 -0500

    Time: 440 ms (88.18%), Space: 0B (100%) - LeetHub

--- 1484-group-sold-products-by-the-date/1484-group-sold-products-by-the-date.sql
+++ 1484-group-sold-products-by-the-date/1484-group-sold-products-by-the-date.sql
@@ -8,7 +8,7 @@ order by sell_date asc
 
 
 #use count distinct cause we want the items number not total number
-select sell_date, count(distinct product) as num_sold,
+select sell_date, count(distinct product) as num_sold, 
     group_concat(distinct product order by product separator ',') as products
 from activities
 group by sell_date
