9e430f33314124f4b21265066551fbaf0a65a46f
William Lin <willyrblin@gmail.com>
2026-09-03 11:19:07 -0500

    Time: 1137 ms (19.64%), Space: 0B (100%) - LeetHub

---
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -0,0 +1,13 @@
+with ranked_salary as(
+    select d.name as Department,
+        e.name as Employee,
+        e.salary as Salary,
+        dense_rank()over(partition by e.departmentId order by e.salary desc) as rank_sal
+    from employee e
+        join department d
+            on e.departmentId = d.id
+)
+
+select Department, Employee, Salary
+from ranked_salary
+where rank_sal = 1
