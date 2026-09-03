f06d0d479ca6b348ba6bf4eca4c2f4dd83995b3e
William Lin <willyrblin@gmail.com>
2026-09-03 11:10:14 -0500

    Time: 669 ms (64.96%), Space: 0B (100%) - LeetHub

--- 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -10,23 +10,12 @@ where (customer_id, order_date) in (
     group by customer_id
 )
 */
-
-select round(avg(case when order_date = customer_pref_delivery_date then 1 else 0 end)*100,2)
-    as immediate_percentage
+select
+    round(avg(case when order_date = customer_pref_delivery_date then 1 else 0 end)*100 ,2) as immediate_percentage
 from delivery
 where (customer_id, order_date) in (
     select customer_id, min(order_date)
     from delivery
     group by customer_id
+
 )
-
-
-
-
-
-
-
-
-
-
-
