d5597fb07590fc286010a655dee1a0c7c5ef93a1
William Lin <willyrblin@gmail.com>
2026-09-16 13:28:33 -0500

    Time: 325 ms (75.69%), Space: 67.2 MB (98.94%) - LeetHub

--- 1741-find-total-time-spent-by-each-employee/1741-find-total-time-spent-by-each-employee.py
+++ 1741-find-total-time-spent-by-each-employee/1741-find-total-time-spent-by-each-employee.py
@@ -12,6 +12,7 @@ def total_time(employees: pd.DataFrame) -> pd.DataFrame:
     '''
     employees['total_time'] = employees['out_time'] - employees['in_time'] #new column
 
+
     #group by event_day and emp_id. Sum up total time
     result = employees.groupby(['event_day','emp_id'], as_index=False)['total_time'].sum()
     result.rename(columns={'event_day':'day'}, inplace=True)
