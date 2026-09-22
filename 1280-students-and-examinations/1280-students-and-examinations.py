15b955b54496b1811c3cd45c082cf71ed0ac8b08
William Lin <willyrblin@gmail.com>
2026-09-22 15:37:43 -0500

    Time: 362 ms (61.7%), Space: 68.6 MB (87.75%) - LeetHub

--- 1280-students-and-examinations/1280-students-and-examinations.py
+++ 1280-students-and-examinations/1280-students-and-examinations.py
@@ -12,14 +12,11 @@ def students_and_examinations(students: pd.DataFrame, subjects: pd.DataFrame, ex
     return result[['student_id', 'student_name', 'subject_name', 'attended_exams']]
 
     '''
-    import pandas as pd
-
-    base_df = students.merge(subjects, how = 'cross')
-    exam_counts = examinations.groupby(['student_id','subject_name']).size().reset_index(name='attended_exams') #extra column: attended_exams
+    
+    base_df = students.merge(subjects,  how='cross')
+    exam_counts = examinations.groupby(['student_id','subject_name']).size().reset_index(name='attended_exams')
 
     result = base_df.merge(exam_counts, on=['student_id','subject_name'], how='left')
-    #left join so keep all on left
-
-    result['attended_exams'] = result['attended_exams'].fillna(0).astype(int)#deal with NA
-    result = result.sort_values(by=['student_id','subject_name'])
+    result['attended_exams'] = result['attended_exams'].fillna(0).astype(int)
+    result = result.sort_values(by = ['student_id','subject_name'])
     return result[['student_id','student_name','subject_name','attended_exams']]
